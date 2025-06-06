7ef573ba9bb9c031fc72597ff4c6ccd009e0c6bd
Tourn Vuthy <vuthytuon168@gmail.com>
2025-06-06 09:14:54 +0700

    Modified: new vercel.json

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const router_1 = require("./router");
-document.addEventListener("DOMContentLoaded", () => {
-    (0, router_1.initRouter)();
-});
